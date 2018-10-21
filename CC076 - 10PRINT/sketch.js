5746ce2479bddb4aa08fd28b41ee71d0df7a1be5
Gonçalo Santos <gregueiras@gmail.com>
2018-10-21 22:25:19 +0100

    Simple prototype

--- CC076 - 10PRINT/sketch.js
+++ CC076 - 10PRINT/sketch.js
@@ -1,1 +1,25 @@
 /*jshint esversion: 6*/
+
+const size = 10;
+let x = 0, y = 0;
+
+function setup() {
+  createCanvas(400, 400);
+  background(33);
+  stroke(255);
+  strokeWeight(size/3);
+}
+
+function draw() {
+  if (random(1) > 0.5) {
+    line(x, y, x + size, y + size);
+  } else {
+    line(x + size, y, x, y + size);
+  }
+  x += size;
+  if (x >= width) {
+    x = 0;
+    y += size;
+  }
+    
+}
